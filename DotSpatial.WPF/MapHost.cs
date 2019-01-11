fa381b3660c8fdbaecced128e132d90d6e4859f2
庞宇 <birderyu@qq.com>
2019-01-11 22:54:01 +0800

    提交了基础代码和基础依赖

---
+++ DotSpatial.WPF/MapHost.cs
@@ -0,0 +1,15 @@
+﻿using DotSpatial.Controls;
+using System.Windows.Forms.Integration;
+
+namespace DotSpatial.WPF
+{
+    public class MapHost : WindowsFormsHost
+    {
+        private readonly Map _map;
+
+        public MapHost()
+        {
+            _map = new Map();
+        }
+    }
+}
